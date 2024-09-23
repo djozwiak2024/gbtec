d4a04a020c885c88f156434e2a1cf4a6bc43c9c6
Dennis Jozwiak <dennis@family-jozwiak.de>
2024-09-23 23:26:56 +0200

    Initial commit

---
+++ src/main/java/com/gbtec/av532/Av532Application.java
@@ -0,0 +1,13 @@
+package com.gbtec.av532;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Av532Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Av532Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gbtec/av532/Av532ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbtec.av532;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Av532ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
